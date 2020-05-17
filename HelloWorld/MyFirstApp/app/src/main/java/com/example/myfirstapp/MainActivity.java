3e6dc2eb46dc0dca7a0c4ae04892a8446c5025b0
Kapilan-Balagopalan <kapilandarth5@gmail.com>
2020-05-17 12:02:23 +0800

    Another Activity

--- HelloWorld/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ HelloWorld/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void sendMessage(android.view.View View){
+        android.content.Intent intent = new android.content.Intent(this, DisplayMessageActivity.class);
+        android.widget.EditText editText = (android.widget.EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
 }
