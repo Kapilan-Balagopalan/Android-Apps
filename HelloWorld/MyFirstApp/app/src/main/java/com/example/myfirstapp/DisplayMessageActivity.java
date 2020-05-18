cb55ae98a25102f2e6d53fb3a7dcc993936db113
Kapilan-Balagopalan <kapilandarth5@gmail.com>
2020-05-18 22:38:37 +0800

    Redirects inside the app no, browers & Connect to a webpage

--- HelloWorld/MyFirstApp/app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
+++ HelloWorld/MyFirstApp/app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -22,7 +23,16 @@ public class DisplayMessageActivity extends AppCompatActivity {
         textView.setText(message);
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("http://www.youtube.com");
+        myWebView.loadUrl("http://c90ebc83.ngrok.io/wa4e/php/crud/index.php");
+
+        myWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                // do your handling codes here, which url is the requested url
+                // probably you need to open that url rather than redirect:
+                view.loadUrl(url);
+                return false; // then it is not handled by default action
+            }
+        });
 
 
     }
